52fd10dc3d30d5761c3af0bfe0fbf42900013821
Avinash Kumar <avinash999yadav@gmail.com>
2020-08-16 14:43:29 +0530

    project setup complete

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,9 @@ import logger from 'redux-logger';
 import reducer from '../reducers';
 
 let store;
+
 export function configureStore() {
   store = createStore(reducer, applyMiddleware(thunk, logger));
+
   return store;
 }
